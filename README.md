da881cb272018607c2079adf53b29466ec84b7ab
Tommi Sinivuo <600815+TommiSinivuo@users.noreply.github.com>
2021-02-14 20:58:51 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,68 @@
+Use-Zig
+=======
+
+Use-Zig is a PowerShell script that can be used for installing multiple Zig
+(ziglang.org) versions and switching between different versions on-the-fly.
+
+I use this script in my own development environments and the current state of
+the script is currently "it works fine for me". For the time being I will be
+updating it based on my personal needs and intend to keep it as simple and
+generic as possible.
+
+## Usage
+
+Install (if not already installed) and use a specific version of Zig:
+
+```
+.\Use-Zig.ps1 0.7.1
+```
+
+Install (if not already installed) and use latest master build:
+
+```
+.\Use-Zig.ps1 master
+```
+
+Update master:
+
+```
+.\Use-Zig.ps1 master -Install
+```
+
+The `-Install` switch basically (re-)installs the specified version, which in
+the case of `master` works like an update, because the latest master
+build will always be downloaded.
+
+### Example session:
+```
+> .\Use-Zig.ps1 0.7.1
+Downloading Zig (0.7.1) from https://ziglang.org/download/0.7.1/zig-windows-x86_64-0.7.1.zip
+Installing Zig (0.7.1) to C:\Users\me\AppData\Local\Zigs\x86_64
+
+> zig version
+0.7.1
+
+> .\Use-Zig.ps1 master
+Downloading Zig (master) from https://ziglang.org/builds/zig-windows-x86_64-0.8.0-dev.1140+9270aae07.zip
+Installing Zig (master) to C:\Users\me\AppData\Local\Zigs\x86_64
+
+> zig version
+0.8.0-dev.1140+9270aae07
+```
+
+## How it works
+
+The Zig versions are installed by default in
+`C:\Users\YOUR_USERNAME\AppData\Local\Zigs`. You can change the location by
+modifying the `$ZIGS_PATH` variable in the script.
+
+A junction (basically a link) is created in
+`C:\Users\YOUR_USERNAME\AppData\Local\Zigs\current` which always points to the
+installation of the Zig version specified when running the script for the last
+time. This junction is added to the Path environment variable of the current
+shell, so that the Zig executable can be used immediately.
+
+When opening a new shell the script needs to be run once for the junction to
+appear in the Path. You can add the path to the junction to your Path
+environment variable to be able to use a Zig installation immediately in a new
+shell.
---
+++ Use-Zig.ps1
@@ -0,0 +1,124 @@
+<#
+.SYNOPSIS
+    Install and switch between different Zig compiler versions.
+.EXAMPLE
+    Use-Zig.ps1 0.7.1
+
+    This command installs Zig version 0.7.1.
+.EXAMPLE
+    Use-Zig.ps1 master
+
+    This command installs the current master of Zig.
+.EXAMPLE
+    Use-Zig.ps1 master -Install
+
+    This command re-installs (updates) master to the latest available build.
+#>
+
+# Change to i386 if you want to use the 32-bit versions instead, but note that
+# master is only available in 64-bit version.
+$ARCH = "x86_64"
+
+# In theory this script could work in Powershell for Linux and macOS as well...
+$PLATFORM = "windows"
+
+# Modify if you want the Zigs to be installed somewhere else than the
+# Appdata\Local folder of the current user.
+$ZIGS_PATH = "$env:USERPROFILE\AppData\Local\Zigs"
+
+# The URL used to fetch the currently available Zig versions.
+$ZIG_VERSIONS_URL = "https://ziglang.org/download/index.json"
+
+$UserVersion = $args[0]
+$ForceInstall = $args[1] -eq "-Install"
+
+if (-not (Test-Path $ZIGS_PATH)) {
+    New-Item -ItemType Directory $ZIGS_PATH | Out-Null
+}
+
+$InstallPath = "$ZIGS_PATH\$ARCH"
+if (-not (Test-Path $InstallPath)) {
+    New-Item -ItemType Directory $InstallPath | Out-Null
+}
+
+$VersionExists = Test-Path "$InstallPath\$UserVersion"
+$ShouldInstall = (-not $VersionExists) -or ($VersionExists -and $ForceInstall)
+
+if ($ShouldInstall) {
+
+    $Response = Invoke-WebRequest -Uri $ZIG_VERSIONS_URL
+    if ($Response.StatusCode -ne 200) {
+        Write-Host "Error: failed to get releases from: $ZIG_VERSION_URL"
+        break
+    }
+
+    $Zigs = $Response.Content | ConvertFrom-Json
+    if (-not $Zigs.$UserVersion) {
+        Write-Host "Error: Zig version $UserVersion not available"
+        break
+    }
+
+    $DownloadUrl = $Zigs.$UserVersion."$ARCH-$PLATFORM".tarball
+    if (-not $DownloadUrl) {
+        "Error: Zig package not found for $ARCH-$PLATFORM and version $userVersion"
+        break
+    }
+
+    $TempPath = "$ZIGS_PATH\temp"
+    if (Test-Path $TempPath) {
+        # The temp folder may exist after an unsuccessfull previous run.
+        # Let's clean up.
+        Remove-Item -Recurse -Force $TempPath
+    }
+    New-Item -ItemType Directory $TempPath | Out-Null
+
+    Write-Host "Downloading Zig ($UserVersion) from $DownloadUrl..."
+    $Response = Invoke-WebRequest -Uri $DownloadUrl -OutFile "$TempPath\zig.zip" -PassThru
+    if ($Response.StatusCode -ne 200) {
+        Write-Host "Error: failed to download package from $DownloadUrl"
+        break
+    }
+
+    if ($VersionPathExists -and $ForceInstall) {
+        Write-Host "Removing previous installation of version $UserVersion"
+        Remove-Item -Force -Recurse "$InstallPath\$UserVersion"
+    }
+
+    Write-Host "Installing Zig ($UserVersion) to $InstallPath"
+    Expand-Archive "$TempPath\zig.zip" -DestinationPath $InstallPath
+
+    Remove-Item -Recurse -Force $TempPath
+
+    # We need the absolute version number to determine the name of the
+    # extracted folder, containing the Zig exe. This is simple for
+    # snapshot versions (like 0.7.1), but the master version is different, bc
+    # it actually has a detailed version number, like 0.8.0-dev.1127+6a5a6386c,
+    # pointing to the latest nightly build. Therefore we need to parse that
+    # version from the downloaded JSON file and use it.
+    if ($UserVersion -eq "master") {
+        $AbsoluteVersion = $zigs.master.version
+    }
+    else {
+        $AbsoluteVersion = $UserVersion
+    }
+    $ExtractedFolderName = "zig-$PLATFORM-$ARCH-$AbsoluteVersion"
+
+    Rename-Item $InstallPath\$ExtractedFolderName $InstallPath\$UserVersion
+
+    $Success = $TRUE
+}
+else {
+    $Success = $TRUE
+}
+
+if ($Success) {
+    $JunctionPath = "$ZIGS_PATH\current"
+    if (Test-Path $JunctionPath) {
+        Remove-Item $JunctionPath
+    }
+    New-Item -ItemType Junction -Path $JunctionPath -Target "$InstallPath\$UserVersion" | Out-Null
+
+    if (-not $env:Path.Contains($JunctionPath)) {
+        $env:Path += ";$JunctionPath"
+    }
+}
